01e54e54952cec39da17c88942a7bf3e4b3f9ca1
zahramirkazemi <zahramirkazemi1997@gmail.com>
2022-02-22 08:57:18 -0800

    init project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SearchBar from './components/SearchBar';
+
+
+class App extends React.Component  {
+  onSearchSubmit(term){
+    console.log(term)
+  }
+
+  render(){
+    return (
+      <div className="ui container" style={{ marginTop: "12px"}}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div>
+    );
+  }
+}
+
+export default App;
